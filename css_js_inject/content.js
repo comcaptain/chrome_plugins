26f17b1b75c2395f5e6ec74ae2cd14c2ebc2ef52
sun mac <htmlcaptain@gmail.com>
2019-01-03 16:16:02 +0800

    Refactor to use js classes introduced in previous commits
    I've tested that the data stored in chrome.storage.sync is in exactly the same format as the previous version

--- css_js_inject/content.js
+++ css_js_inject/content.js
@@ -1,41 +1,60 @@
-function updateCss(css) {
-	if (css) css = css.trim();
-	var style = document.querySelector("style#sgqInjectCss");
-	if (style === null) {
-		if (css === "") return;
-		style = document.createElement("style");
-		style.type = "text/css";
-		style.id = "sgqInjectCss";
-		style.classList.add("sgqInjectMark");
-		document.head.appendChild(style);
+(function()
+{
+	const URL_ALIASES =
+	{
+		"bootstrap": "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"
 	}
-	style.innerHTML = css;
-}
-var urlAlias = {
-	"bootstrap": "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"
-}
-function loadExternalCss(urls) {
-	if (!urls) return;
-	urls.forEach(function(url) {
-		if (urlAlias[url] != undefined) url = urlAlias[url];
-		var link = document.createElement("link");
-		link.rel = "stylesheet";
-		link.href = url;
-		link.classList.add("sgqInjectMark");
-		document.head.appendChild(link);
-	});
-}
-getInjectDataFromStorage().then(function(injectionData) {
-	if (injectionData == undefined) return;
-	updateCss(injectionData.cssInjection);
-	loadExternalCss(injectionData.externalCssInjection);
-	eval(injectionData.jsInjection);
-	document.addEventListener("DOMContentLoaded", function() {
-		Array.prototype.forEach.call(document.querySelectorAll(".sgqInjectMark"), function(ele) {
-			document.head.removeChild(ele);
-			document.head.appendChild(ele);
+	let injectedDataStorage = new InjectedDataStorage(new DataStorage());
+
+	function injectCSS(cssCode)
+	{
+		if (cssCode) cssCode = cssCode.trim();
+		var style = document.querySelector("style#sgq-injected-css");
+		if (style === null)
+		{
+			if (cssCode === "") return;
+			style = document.createElement("style");
+			style.type = "text/css";
+			style.id = "sgq-injected-css";
+			style.classList.add("sgq-injected-css");
+			document.head.appendChild(style);
+		}
+		style.innerHTML = cssCode;
+	}
+
+	function injectExternalStyleSheets(urls)
+	{
+		if (!urls) return;
+		urls.forEach(function(url)
+		{
+			if (URL_ALIASES[url] != undefined) url = URL_ALIASES[url];
+			var link = document.createElement("link");
+			link.rel = "stylesheet";
+			link.href = url;
+			link.classList.add("sgq-injected-css");
+			document.head.appendChild(link);
 		});
-	});
-});
+	}
 
+	injectedDataStorage.load().then(injectionData =>
+	{
+		if (!injectionData) return;
 
+		// Do injection
+		injectCSS(injectionData.cssCode);
+		injectExternalStyleSheets(injectionData.cssURLs);
+		eval(injectionData.jsCode);
+
+		// re-append injected css to guarantee that it comes after page's existing css code
+		document.addEventListener("DOMContentLoaded", function()
+		{
+			Array.prototype.forEach.call(document.querySelectorAll(".sgq-injected-css"), function(ele)
+			{
+				document.head.removeChild(ele);
+				document.head.appendChild(ele);
+			});
+		});
+	})
+
+	window.injectCSS = injectCSS;
+})()
--- css_js_inject/main.js
+++ css_js_inject/main.js
@@ -1,78 +1,117 @@
-var tab;
-var jsEditor, cssEditor, externalCssEditor;
-var storage = chrome.storage.sync;
-function redrawEditor(editor) {
-	editor.resize();
-	editor.renderer.updateFull();
-	editor.focus();
-}
-function bindListeners() {
-	document.querySelector("ul.tabs").addEventListener("click", function(event) {
-		if (!event.target.classList.contains("tab")) return;
-		var selectedTabEle = event.target;
-		Array.prototype.forEach.call(this.querySelectorAll(".tab"), function(tabEle) {
-			var targetEle = document.getElementById(tabEle.getAttribute("tab-content-id"));
-			if (tabEle === selectedTabEle) {
-				tabEle.classList.add("selected");
-				targetEle.style.display = "block";
+(function()
+{
+	let jsEditor, cssEditor, externalCSSEditor;
+	let dataStorage = new InjectedDataStorage(new DataStorage());
+
+	function initializeEditors()
+	{
+		externalCSSEditor = ace.edit("external-css-editor");
+		externalCSSEditor.setTheme("ace/theme/monokai");
+
+		jsEditor = ace.edit("js-editor");
+		jsEditor.setTheme("ace/theme/monokai");
+		jsEditor.getSession().setMode("ace/mode/javascript");
+
+		cssEditor = ace.edit("css-editor");
+		cssEditor.setTheme("ace/theme/monokai");
+		cssEditor.getSession().setMode("ace/mode/css");
+	}
+
+	function activateTabSwitch()
+	{
+		document.querySelector("ul.tabs").addEventListener("click", function(event) {
+			if (!event.target.classList.contains("tab")) return;
+			let selectedTabEle = event.target;
+			Array.prototype.forEach.call(this.querySelectorAll(".tab"), function(tabEle)
+			{
+				let targetEle = document.getElementById(tabEle.getAttribute("tab-content-id"));
+				if (tabEle === selectedTabEle)
+				{
+					tabEle.classList.add("selected");
+					targetEle.style.display = "block";
+				}
+				else
+				{
+					tabEle.classList.remove("selected");
+					targetEle.style.display = "none";
+				}
+			});
+			if (selectedTabEle.id === "cssTab")
+			{
+				redrawEditor(cssEditor);
 			}
-			else {
-				tabEle.classList.remove("selected");
-				targetEle.style.display = "none";
+			else if (selectedTabEle.id === "jsTab")
+			{
+				redrawEditor(jsEditor);
 			}
+			else
+			{
+				redrawEditor(externalCSSEditor);
+			}
+		});	
+	}
+
+	function activateAutoSave()
+	{
+		cssEditor.on("change", function() {
+			injectCSS();
+			saveEditorContent();
 		});
-		if (selectedTabEle.id === "cssTab") {
-			redrawEditor(cssEditor);
-		}
-		else if (selectedTabEle.id === "jsTab"){
-			redrawEditor(jsEditor);
-		}
-		else {
-			redrawEditor(externalCssEditor);
-		}
-	});
-	cssEditor.on("change", function() {
-		injectCss();
-		saveEditorContent();
-	});
-	jsEditor.on("change", function() {
-		saveEditorContent();
-	});
-	externalCssEditor.on("change", function() {
-		saveEditorContent();
-	});
-}
-document.addEventListener("DOMContentLoaded", function() {
-	externalCssEditor = ace.edit("externalCssEditor");
-	externalCssEditor.setTheme("ace/theme/monokai");
-	jsEditor = ace.edit("jsEditor");
-	jsEditor.setTheme("ace/theme/monokai");
-	jsEditor.getSession().setMode("ace/mode/javascript");
-	cssEditor = ace.edit("cssEditor");
-	cssEditor.setTheme("ace/theme/monokai");
-	cssEditor.getSession().setMode("ace/mode/css");
-	chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-		window.tab = tabs[0];
-		loadEditorContent().then(bindListeners);
+		jsEditor.on("change", function() {
+			saveEditorContent();
+		});
+		externalCSSEditor.on("change", function() {
+			saveEditorContent();
+		});		
+	}
+
+	function redrawEditor(editor)
+	{
+		editor.resize();
+		editor.renderer.updateFull();
+		editor.focus();
+	}
+
+	document.addEventListener("DOMContentLoaded", function()
+	{
+		initializeEditors();
+
+		chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+			// Save current tab to get correct url in InjectedDataStorage
+			window.tab = tabs[0];
+			loadEditorContent().then(() =>
+			{
+				activateTabSwitch();
+				activateAutoSave();
+			});
+		})
 	})
-});
-function loadEditorContent() {
-	return getInjectDataFromStorage().then(function(injectionData) {
-		if (injectionData == undefined) return;
-		jsEditor.setValue(injectionData.jsInjection);
-		cssEditor.setValue(injectionData.cssInjection);
-		externalCssEditor.setValue(injectionData.externalCssInjection.join("\n"));
-	});
-}
-function saveEditorContent() {
-	saveInjectDataToStorage(jsEditor.getValue(), cssEditor.getValue(), externalCssEditor.getValue().split(/\s*\n\s*/));
-}
-function injectCss() {
-	var value = escapeCode(cssEditor.getValue());
-	chrome.tabs.executeScript({
-		code: "updateCss('" + value + "');",
-	});
-}
-function escapeCode(code) {
-	return code.replace(/'/g, "\'").replace(/\r?\n/g, "\\n");
-}
+
+	function loadEditorContent()
+	{
+		return dataStorage.load().then(function(injectionData) {
+			if (!injectionData) return;
+			jsEditor.setValue(injectionData.jsCode);
+			cssEditor.setValue(injectionData.cssCode);
+			externalCSSEditor.setValue(injectionData.cssURLs.join("\n"));
+		});
+	}
+
+	function saveEditorContent()
+	{
+		dataStorage.save(new InjectedData(cssEditor.getValue(), jsEditor.getValue(), externalCSSEditor.getValue().split(/\s*\n\s*/)));
+	}
+
+	function injectCSS()
+	{
+		let value = escapeCode(cssEditor.getValue());
+		chrome.tabs.executeScript({
+			code: "injectCSS('" + value + "');",
+		});
+	}
+
+	function escapeCode(code)
+	{
+		return code.replace(/'/g, "\'").replace(/\r?\n/g, "\\n");
+	}
+})()
