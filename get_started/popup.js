9c9afc489678fb0b916d3f3c947468a34f2f68cb
Tony <htmlcaptain@gmail.com>
2020-12-13 20:00:44 +0900

    Change color on click

--- get_started/popup.js
+++ get_started/popup.js
@@ -4,3 +4,13 @@ chrome.storage.sync.get('color', data =>
 	changeColor.style.backgroundColor = data.color;
 	changeColor.setAttribute("value", data.color);
 })
+
+changeColor.addEventListener("click", e =>
+{
+	const color = e.target.value;
+	chrome.tabs.query({ active: true, currentWindow: true }, tabs =>
+	{
+		const tab = tabs[0];
+		chrome.tabs.executeScript(tab.id, { code: `document.body.style.background = "${color}";` });
+	})
+})
