2da9b81c931b10ebdf74e637ad10940d4fae61f9
Tony <htmlcaptain@gmail.com>
2020-12-13 20:43:06 +0900

    Print thread list

--- better-web-world/sis/亚洲无码原创.js
+++ better-web-world/sis/亚洲无码原创.js
@@ -1,1 +1,4 @@
-console.info(document.title);
+const THREAD_LINK_SELECTOR = "tbody[id^=normalthread] span[id^=thread] > a[href^=thread-]";
+const infoPre = document.createElement("pre");
+infoPre.textContent = [].slice.apply(document.querySelectorAll(THREAD_LINK_SELECTOR)).map(a => a.textContent).join("\n");
+document.body.prepend(infoPre);
